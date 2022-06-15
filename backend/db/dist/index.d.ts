f5b4b9a9cab5b9d37bab645d0ce26a6e4ee53721
system32uwu <mateocarriqui7@gmail.com>
2022-06-15 00:36:50 -0300

    chore: fix linter errors, update matrix osses

---
+++ backend/db/dist/index.d.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from '@prisma/client';
+declare let prisma: PrismaClient;
+export default prisma;
+export * from '@prisma/client';
--- frontend/landing/components/layout/Section.tsx
+++ frontend/landing/components/layout/Section.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
 
 type ISectionProps = {
-  title?: string;
-  description?: string;
-  yPadding?: string;
-  children: ReactNode;
-};
+  title?: string
+  description?: string
+  yPadding?: string
+  children: ReactNode
+}
 
 const Section = (props: ISectionProps) => (
   <div
@@ -16,16 +16,20 @@ const Section = (props: ISectionProps) => (
     {(props.title || props.description) && (
       <div className="mb-12 text-center">
         {props.title && (
-          <h2 className="text-4xl text-gray-900 font-bold">{props.title}</h2>
+          <h2 className="text-4xl text-gray-900 font-bold">
+            {props.title}
+          </h2>
         )}
         {props.description && (
-          <div className="mt-4 text-xl md:px-20">{props.description}</div>
+          <div className="mt-4 text-xl md:px-20">
+            {props.description}
+          </div>
         )}
       </div>
     )}
 
     {props.children}
   </div>
-);
+)
 
-export { Section };
+export { Section }
