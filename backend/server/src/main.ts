7fecb4810da9ecc5687b67658530090494389210
system32uwu <mateocarriqui7@gmail.com>
2022-06-18 03:11:04 -0300

    chore: remove TODO

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-// TODO: Detect environment and apply CORS policy to whitelisted frontends only
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT || 4000)
