23755ba8c591bae4f71b71127dbd10960a37f29d
system32uwu <mateocarriqui7@gmail.com>
2022-06-18 20:58:12 -0300

    fix: force install node on eb

--- _config/.platform/hooks/prebuild/yarn.sh
+++ _config/.platform/hooks/prebuild/yarn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # need to install node first to be able to install yarn (as at prebuild no node is present yet)
-node -v || (curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - && yum -y install nodejs)
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - && yum -y install nodejs
 
 yarn -v || (wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo && yum -y install yarn)
 
